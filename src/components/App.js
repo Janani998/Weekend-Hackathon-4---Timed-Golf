4dfa3610bbe15479dcdc27a77e6a42223a9f5fc3
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-11-13 23:26:03 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,32 +5,19 @@ class Timer extends React.Component {
     super(props);
     this.timer = 0;
     this.state = { time: 0, x: 0, y: 0, startGame: false };
+    this.onKeyDown = this.onKeyDown.bind(this);
   }
 
   componentDidMount() {
-    const onKeyDown = function (event) {
-      if (event.keyCode === 39) {
-        this.setState({ x: this.state.x + 5 });
-      }
-      if (event.keyCode === 37) {
-        this.setState({ x: this.state.x - 5 });
-      }
-      if (event.keyCode === 38) {
-        this.setState({ y: this.state.y - 5 });
-      }
-      if (event.keyCode === 40) {
-        this.setState({ y: this.state.y + 5 });
-      }
-    };
-    this.timer = setInterval(function () {
+    this.timer = setInterval(() => {
       if (this.state.startGame) {
         this.setState({ time: this.state.time + 1 });
         if (this.state.x === 250 && this.state.y === 250) {
           this.setState({ startGame: false });
-          document.removeEventListener("keydown", onKeyDown);
+          document.removeEventListener("keydown", this.onKeyDown);
           return;
         }
-        document.addEventListener("keydown", onKeyDown);
+        document.addEventListener("keydown", this.onKeyDown);
       }
     }, 1000);
   }
@@ -38,6 +25,21 @@ class Timer extends React.Component {
   componentWillUnmount() {
     clearInterval(this.timer);
   }
+
+  onKeyDown = function (event) {
+    if (event.keyCode === 39) {
+      this.setState({ x: this.state.x + 5 });
+    }
+    if (event.keyCode === 37) {
+      this.setState({ x: this.state.x - 5 });
+    }
+    if (event.keyCode === 38) {
+      this.setState({ y: this.state.y - 5 });
+    }
+    if (event.keyCode === 40) {
+      this.setState({ y: this.state.y + 5 });
+    }
+  };
 
   handleClick = function () {
     this.setState({ startGame: true });
